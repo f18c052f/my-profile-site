ef009ff84c84356eeb75d1a09f07c524d4bb40e8
DESKTOP-VGKBO16\digit <digitalwrite01@gmail.com>
2024-09-29 12:11:10 +0900

    CSS Module修正

--- src/app/[lang]/_home/Top.tsx
+++ src/app/[lang]/_home/Top.tsx
@@ -1,47 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import { ClipLoader } from 'react-spinners'; // スピナーライブラリをインポート
 import iconImage from '@/assets/images/top/icon.jpg';
 import backgroundImage from '@/assets/images/top/background.jpg';
 import githubImage from '@/assets/images/top/github-icon.svg';
+import styles from '@/assets/css/Top.module.css';
 
 const Top: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
   return (
-    <div className="relative h-[70vh] w-full">
+    <div className={styles.container}>
+      {/* ローディング中にスピナーを表示 */}
+      {loading && (
+        <div className={styles.spinnerContainer}>
+          <ClipLoader color="#ffffff" loading={loading} size={50} />
+        </div>
+      )}
+
       <Image
         src={backgroundImage}
         alt="Background Image"
         fill
-        style={{ objectFit: 'cover', zIndex: -1 }}
+        className={styles.backgroundImage}
         priority
+        onLoad={handleImageLoad}
       />
 
-      <div className="flex h-full items-center justify-center">
-        <div
-          className="flex flex-col items-center justify-center bg-white bg-opacity-10 p-16 text-center font-bold text-white backdrop-blur-md"
-          style={{ textShadow: '0 0 20px black' }}
-        >
+      <div className={styles.content}>
+        <div className={styles.overlay}>
           <div>
-            <Image
-              src={iconImage}
-              alt="Yuta Ohara"
-              className="rounded-full"
-              width={128}
-              height={128}
-            />
+            <Image src={iconImage} alt="Yuta Ohara" className={styles.icon} />
           </div>
-          <h1 className="text-4xl font-semibold">Yuta Ohara</h1>
-          <div className="mt-4">
+          <h1 className={styles.title}>Yuta Ohara</h1>
+          <div className={styles.githubIcon}>
             <a
               href="https://github.com/f18c052f"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                src={githubImage}
-                alt="GitHub"
-                style={{ width: '40px', height: 'auto' }}
-                priority
-              />
+              <Image src={githubImage} alt="GitHub" priority />
             </a>
           </div>
         </div>
